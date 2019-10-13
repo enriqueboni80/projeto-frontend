988aeac02a219ca121f7b06c4b7abb305772a994
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-10-13 15:39:23 -0300

    # Alterando o projeto para GEOLOCATION COM LOCALSTORAGE

--- js/main.js
+++ js/main.js
@@ -1,7 +1,67 @@
-function helloWorld() {
-    for (let i = 3; i > 0; i--) {
-        console.log(i);
+var x = document.getElementById("demo");
+var lat
+var long
+var posicoes = []
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
-    console.log('...');
-    console.log('Hello World!');
-} helloWorld();
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude
+    long = position.coords.longitude
+    exibirLocalizacao()
+}
+
+function gravarLocalizacao() {
+    posicoes = getPosicoes()
+    posicao = {
+        data: getToday(),
+        latitude: lat,
+        longitude: long
+    }
+    posicoes.push(posicao)
+    posicoes = JSON.stringify(posicoes)
+    localStorage.setItem("posicao", posicoes)
+    exibirLocalizaoNaTabela()
+}
+
+function exibirLocalizacao() {
+    x.innerHTML = "Latitude: " + lat +
+        "<br>Longitude: " + long;
+}
+
+function exibirLocalizaoNaTabela() {
+    posicoes = getPosicoes()
+    $(".exibir-localizacao-tabela").html("")
+    $.each(posicoes, function(index) {
+        $(".exibir-localizacao-tabela").append(
+            `<tr>
+                <td>${posicoes[index].data}</td>
+                <td>${posicoes[index].latitude}</td>
+                <td>${posicoes[index].longitude}</td>
+                <td>@mdo</td>
+            </tr>`
+        )
+    })
+}
+
+function getPosicoes() {
+    posicoes = JSON.parse(localStorage.getItem("posicao"))
+    if (posicoes == null) {
+        return posicoes = []
+    }
+    return posicoes
+}
+
+function getToday() {
+    return new Date(Date.now());
+}
+
+$(document).ready(function() {
+    exibirLocalizaoNaTabela()
+})
