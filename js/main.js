8baf2909549152197ee4c9de67569d7baa8147a9
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-10-21 22:10:57 -0300

    # comecando a fazer o google maps funcionar

--- js/main.js
+++ js/main.js
@@ -136,4 +136,17 @@ function deletarRegistro(numeroRegistro) {
 
 $(document).ready(function() {
     exibirLocalizaoNaTabela()
+    initMap()
 })
+
+
+
+
+
+function initMap() {
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
