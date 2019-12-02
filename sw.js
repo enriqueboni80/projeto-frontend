60e01105a5c754c6b2524893d6ee58dfad495f47
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-12-02 14:19:27 -0300

    # Configurando Service Workers

---
+++ sw.js
@@ -0,0 +1,37 @@
+const cacheName = "teste";
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                    `/`,
+                    `/index.html`,
+                    `/dist/css/normalize.css`,
+                    `/dist/css/all.css`,
+                    `/dist/css/main.css`,
+                    `/dist/js/vendor/jquery-3.4.1.min.js`,
+                    `/dist/js/plugins.js`,
+                    `/dist/js/bundle.js`,
+                    `/dist/images/backgrounds/background.jpg`,
+                    `/dist/webfonts/fa-solid-900.woff2`,
+                    `/dist/webfonts/fa-solid-900.woff`,
+                    `/dist/webfonts/fa-solid-900.ttf`
+                ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(cache => cache.match(event.request, { ignoreSearch: true }))
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
